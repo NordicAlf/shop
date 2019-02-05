aa2d28e3956d2a854573ee0e6443bd4b71c6f22e
Andrew <sver4ok@protonmail.com>
2019-02-05 20:18:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,6 @@ session_start();
 // 2)Подключения файлов
 require_once(ROOT.'/components/Autoload.php'); // Автозагрузка классов
 
-// 4)Вызов Router
+// 3)Вызов Router
 $router = new Router();
 $router->run();
